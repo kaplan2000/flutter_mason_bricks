0f7826cbcd8599edc653c041547a7c73964040b2
Mustafa Kaplan <kaplan@Mustafa-MacBook-Pro.local>
2025-12-28 20:12:09 +0300

    Version: 0.1.1-fix

--- bricks/flow/hooks/post_gen.dart
+++ bricks/flow/hooks/post_gen.dart
@@ -1,12 +1,14 @@
 import 'dart:io';
 import 'package:mason/mason.dart';
 
+// Version: 0.1.1-fix
 void run(HookContext context) {
   final featureName = context.vars['feature_name'] as String;
   final flowName = context.vars['flow_name'] as String;
   final hasRemote = context.vars['has_remote'] as bool;
   final hasMock = context.vars['has_mock'] as bool;
 
+  // Using the snakeCase extension from package:mason
   final featureSnake = featureName.snakeCase;
   final flowSnake = flowName.snakeCase;
 
